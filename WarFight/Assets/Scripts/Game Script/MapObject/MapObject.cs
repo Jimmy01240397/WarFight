6525a2f6917837d9f1e9b1f87e9fdd37c06470b4
蔡聿善 <jimmy012403976@gmail.com>
2023-01-05 23:19:32 +0800

    fix thing

--- WarFight/Assets/Scripts/Game Script/MapObject/MapObject.cs
+++ WarFight/Assets/Scripts/Game Script/MapObject/MapObject.cs
@@ -1,9 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MapObject : MonoBehaviour
 {
+    public int ID { get; private set; }
+    public int OwnerIndex { get; private set; }
+    public float MaxLife { get; private set; }
+    public float Life
+    {
+        get
+        {
+            return lifeline.value * MaxLife;
+        }
+        set
+        {
+            lifeline.value = value / MaxLife;
+        }
+    }
+
+    Slider lifeline;
+
+    private void Awake()
+    {
+        if(transform.Find("lifeline") == null)
+        {
+            GameObject lifelineobj = Instantiate(Resources.Load<GameObject>("Prefabs/Game/lifeline"));
+            lifelineobj.transform.SetParent(transform, false);
+        }
+        lifeline = GetComponentInChildren<Slider>();
+        Debug.Log(GetComponent<SpriteRenderer>().bounds.size);
+        lifeline.GetComponent<RectTransform>().sizeDelta = new Vector2(GetComponent<SpriteRenderer>().bounds.size.x, 3);
+        lifeline.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, GetComponent<SpriteRenderer>().bounds.size.y / 2 + 4);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
